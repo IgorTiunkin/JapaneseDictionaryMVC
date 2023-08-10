2fa8145dae887eec2b04b93c8781c6034fda1443
Igor Tiunkin <fantomenergy@gmail.com>
2023-08-10 21:34:03 +0300

    Base pom created

---
+++ src/main/java/com/phantom/japanese_dictionary_mvc/JapaneseDictionaryMvcApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.japanese_dictionary_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseDictionaryMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapaneseDictionaryMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phantom/japanese_dictionary_mvc/JapaneseDictionaryMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom.japanese_dictionary_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseDictionaryMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
