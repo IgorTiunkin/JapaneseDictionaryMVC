6ad7338281b8dcd15159fde5c7cee7f8f7d2cc10
Igor Tiunkin <fantomenergy@gmail.com>
2023-08-18 22:57:49 +0300

    Base Logger added

---
+++ src/main/java/com/phantom/japanese_dictionary_mvc/util/RequestResponseLoggingFilter.java
@@ -0,0 +1,32 @@
+package com.phantom.japanese_dictionary_mvc.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.LogRecord;
+
+@Component
+@Slf4j
+public class RequestResponseLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        log.info(
+                "Logging Request  {} : {}", req.getMethod(),
+                req.getRequestURI());
+        chain.doFilter(request, response);
+        log.info(
+                "Logging Response :{}",
+                res.getContentType());
+    }
+}
