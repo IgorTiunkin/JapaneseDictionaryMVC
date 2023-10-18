ec03a380eeb4d135923f2e30820a38320ac38469
Igor Tiunkin <fantomenergy@gmail.com>
2023-10-18 21:06:37 +0300

    Added QuizConverterTest test

--- src/test/java/com/phantom/japanese_dictionary_mvc/util/QuizConverterTest.java
+++ src/test/java/com/phantom/japanese_dictionary_mvc/util/QuizConverterTest.java
@@ -1,7 +1,9 @@
 package com.phantom.japanese_dictionary_mvc.util;
 
+import com.phantom.japanese_dictionary_mvc.models.Note;
 import com.phantom.japanese_dictionary_mvc.models.QuizTask;
 import com.phantom.japanese_dictionary_mvc.requests.QuizRequest;
+import com.phantom.japanese_dictionary_mvc.requests.RequestType;
 import com.phantom.japanese_dictionary_mvc.services.NoteService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
@@ -24,14 +27,39 @@ class QuizConverterTest {
     @InjectMocks
     private QuizConverter quizConverter;
 
-    private final QuizTask TEST_QUIZ_TASk1 = new QuizTask(1, "q1", "a1", List.of("o1", "o2"));
-    private final QuizTask TEST_QUIZ_TASk2 = new QuizTask(2, "q2", "a2", List.of("o3", "o4"));
+    private final Note TEST_NOTE_YAMAI = new Note(1, "yamai", "病", "やまい","болезнь");
+    private final Note TEST_NOTE_JOUDAN = new Note(2, "joudan", "冗談", "じょうだん","шутка");
+    private final Note TEST_NOTE_SENSOU = new Note(3, "sensou", "戦争", "せんそう","войн");
+    private final Note TEST_NOTE_KUMO = new Note(4, "kumo", "蜘蛛", "くも","паук");
 
     @Test
-    public void when_then() {
+    public void whenTranslation_thenKanji() {
+        int numberOfTasks = 2;
+        int numberOfOptions = 2;
         QuizRequest quizRequest = new QuizRequest();
-        quizRequest.setNumberOfTasks(2);
-        quizRequest.setNumberOfOptions(2);
+        quizRequest.setNumberOfTasks(numberOfTasks);
+        quizRequest.setNumberOfOptions(numberOfOptions);
+        quizRequest.setRequestType(RequestType.TRANSLATION);
+        doReturn(List.of(TEST_NOTE_YAMAI, TEST_NOTE_JOUDAN, TEST_NOTE_SENSOU, TEST_NOTE_KUMO))
+                .when(noteService).getRandomVariants(4);
+        assertEquals(numberOfTasks,quizConverter.getQuizTasks(quizRequest).size());
+        assertEquals(numberOfOptions, quizConverter.getQuizTasks(quizRequest).get(0).getOptions().size());
+        assertEquals(TEST_NOTE_YAMAI.getKanji(), quizConverter.getQuizTasks(quizRequest).get(0).getRightAnswer());
+    }
+
+    @Test
+    public void whenKanji_thenTranslation() {
+        int numberOfTasks = 2;
+        int numberOfOptions = 2;
+        QuizRequest quizRequest = new QuizRequest();
+        quizRequest.setNumberOfTasks(numberOfTasks);
+        quizRequest.setNumberOfOptions(numberOfOptions);
+        quizRequest.setRequestType(RequestType.KANJI);
+        doReturn(List.of(TEST_NOTE_YAMAI, TEST_NOTE_JOUDAN, TEST_NOTE_SENSOU, TEST_NOTE_KUMO))
+                .when(noteService).getRandomVariants(4);
+        assertEquals(numberOfTasks,quizConverter.getQuizTasks(quizRequest).size());
+        assertEquals(numberOfOptions, quizConverter.getQuizTasks(quizRequest).get(0).getOptions().size());
+        assertEquals(TEST_NOTE_YAMAI.getTranslation(), quizConverter.getQuizTasks(quizRequest).get(0).getRightAnswer());
     }
 
 }
