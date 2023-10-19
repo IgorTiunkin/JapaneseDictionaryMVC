0aac9319cadaa079962de02b9743d8f5d85f6465
Igor Tiunkin <fantomenergy@gmail.com>
2023-10-19 23:25:07 +0300

    Added GrammarNoteServiceIT and PeopleServiceIT

---
+++ src/test/java/com/phantom/japanese_dictionary_mvc/integration/GrammarNoteServiceIT.java
@@ -0,0 +1,45 @@
+package com.phantom.japanese_dictionary_mvc.integration;
+
+import com.phantom.japanese_dictionary_mvc.models.GrammarNote;
+import com.phantom.japanese_dictionary_mvc.models.Note;
+import com.phantom.japanese_dictionary_mvc.services.GrammarNoteService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GrammarNoteServiceIT extends BaseServiceIT{
+
+    private final GrammarNoteService grammarNoteService;
+
+    private final GrammarNote TEST_GRAMMAR_NOTE
+            = new GrammarNote(1, "Test source", "Test rule",
+            "Test explanation", "Test example");
+
+    @Autowired
+    public GrammarNoteServiceIT(GrammarNoteService grammarNoteService) {
+        this.grammarNoteService = grammarNoteService;
+    }
+
+    @Test
+    public void whenToShite_then3Partial() {
+        String wordToFind = "to shite";
+        List<GrammarNote> grammarNotes = grammarNoteService.findByEnglishFragment(wordToFind);
+        assertEquals(3, grammarNotes.size());
+        for (GrammarNote note : grammarNotes) {
+            assertTrue(note.getRule().contains(wordToFind)||note.getExample().contains(wordToFind));
+        }
+        assertTrue(grammarNotes.get(0).getRule().contains(wordToFind));
+        assertTrue(grammarNotes.get(2).getExample().contains(wordToFind)
+                && !grammarNotes.get(2).getRule().contains(wordToFind));
+    }
+
+    @Test
+    public void whenSave_thenTrue() {
+        boolean saveNote = grammarNoteService.saveGrammarNote(TEST_GRAMMAR_NOTE);
+        assertTrue(saveNote);
+    }
+}
---
+++ src/test/java/com/phantom/japanese_dictionary_mvc/integration/PeopleServiceIT.java
@@ -0,0 +1,46 @@
+package com.phantom.japanese_dictionary_mvc.integration;
+
+import com.phantom.japanese_dictionary_mvc.models.Person;
+import com.phantom.japanese_dictionary_mvc.models.QuizResult;
+import com.phantom.japanese_dictionary_mvc.services.PeopleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PeopleServiceIT extends BaseServiceIT{
+
+    private final PeopleService peopleService;
+
+    private final Person PERSON = new Person(1, "username", "password", "admin",
+            new ArrayList<QuizResult>());
+
+    @Autowired
+    public PeopleServiceIT(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @Test
+    public void whenSave_thenTrue() {
+        boolean saveUser = peopleService.saveUser(PERSON);
+        assertTrue(saveUser);
+    }
+
+    @Test
+    public void whenPresent_thenPresent() {
+        Optional<Person> user = peopleService.getUserByUsername("user");
+        assertTrue(user.isPresent());
+    }
+
+    @Test
+    public void whenAbsent_thenEmpty() {
+        Optional<Person> user = peopleService.getUserByUsername("test");
+        assertTrue(user.isEmpty());
+    }
+
+
+}
